cdf3cdb49244b0c8c05d4e4d5b63f876551b602b
Chenxin <55467785+chenxin-chex@users.noreply.github.com>
2022-04-01 17:01:50 +0000

    add comments to selection sort

--- SelectSort.java
+++ SelectSort.java
@@ -5,14 +5,17 @@ public class SelectSort extends BaseSort {
 
   public void sort() {
     int n = unsorted.length;
+    // iterate through each element of list as starting index
     for (int i = 0; i < n - 1; i++) {
       int min = i;
+      // within this sublist that has starting index of i, find the minimum value
       for (int j = i+1; j < n; j++) {
         compare++;
         if (unsorted[j] < unsorted[min]) {
           min = j;
         }
       }
+      // sort minimum value to the start of the list.
       int temp = unsorted[min];
       unsorted[min] = unsorted[i];
       unsorted[i] = temp;
