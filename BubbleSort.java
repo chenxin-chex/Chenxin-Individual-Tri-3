37f4c26de4090de9d01c9708daf509a80a3b3de0
Chenxin <55467785+chenxin-chex@users.noreply.github.com>
2022-04-01 17:03:31 +0000

    add comments to bubble sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,9 +6,11 @@ public class BubbleSort extends BaseSort {
   
   public void sort() {
     int n = unsorted.length;
+    // iterate through each possible pair in the list
     for (int i = 0; i < n - 1; i++) {
       for (int j = 0; j < n - 1 - i; j++) {
         compare++;
+        // if the value with larger index is smaller in value, then swap the two values
         if (unsorted[j] > unsorted[j+1]) {
           sort++;
           int temp = unsorted[j];
